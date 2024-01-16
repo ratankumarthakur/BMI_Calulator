9f936ceb08e54d8aa0b1f8403528c4a94a23a046
ratankumarthakur <144756277+ratankumarthakur@users.noreply.github.com>
2024-01-16 19:11:41 +0530

    main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
+//What's new I learnt 
+//to display different types of data/answers
+//to give color to elevated button
+//need to convert TextEditingController 1st to string than to any other datatype
+//convert one datatype to any other datatype
+//when to use and when not to use setState function
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -23,42 +30,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var wtcontroller=TextEditingController();
-  var ftcontroller=TextEditingController();
-  var incontroller=TextEditingController();
-  var result="";
-  var bgcolor=Colors.purple.shade200;
+  var wtcontroller=TextEditingController();     //for taking weight as input
+  var ftcontroller=TextEditingController();     //for taking height in feet as input
+  var incontroller=TextEditingController();     //for taking height in inches as input
+  var result="";                                //this will store the message to display (underweight,overweight,healthy)
+  var bgcolor=Colors.purple.shade200;           //for changing the color of backgroud wrt different health situations
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("BODY MASS INDEX"),centerTitle: true,backgroundColor: Colors.purple,
-        ),body: Container(
+          title: const Text("BODY MASS INDEX"),
+          centerTitle: true,backgroundColor: Colors.purple,
+        ),
+      body: Container(
       color: bgcolor,
       child: Center(
         child: Container(
           width: 300,
           child: Column(
-            //crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Container(
-              height: 20,
-            ),
+            children: [
+              Container(
+              height: 20),                                     //generally we use SizedBox(height:20) to give spacing between different widgets
               Image.asset("assets/images/img_1.png",width: 100,height: 100),
-
               TextField(
                 controller: wtcontroller,
                 decoration: const InputDecoration(
                     label: Text("enter your weight in kg"),
                     labelStyle: TextStyle(color: Colors.purple),
                     prefixIcon: FaIcon(FontAwesomeIcons.weightScale,size: 20,color: Colors.purple,)
-                ),keyboardType: TextInputType.number,
+                ),keyboardType: TextInputType.number,    //this works/useful in phones only as in laptop no one restrict us to use any key of keyboard
               ),
               Container(
-                height: 10,
-              ),
-
+                height: 10),
               TextField(
                 controller: ftcontroller,
                 decoration: const InputDecoration(
@@ -70,7 +75,6 @@ class _HomePageState extends State<HomePage> {
               Container(
                 height: 10,
               ),
-
               TextField(
                 controller: incontroller,
                 decoration: const InputDecoration(
@@ -82,15 +86,16 @@ class _HomePageState extends State<HomePage> {
               Container(
                 height: 50,
               ),
-              ElevatedButton(style: ElevatedButton. styleFrom(primary: Colors.purple),
-                child: const Text('Calculate',style:Textstyle(color:Colors.purple),
+              ElevatedButton(
+                style: ElevatedButton. styleFrom(primary: Colors.purple),         //this is used to give color to elevated button,to adjust height and width of 
+                child: const Text('Calculate',style:Textstyle(color:Colors.purple),  //elevated button wrap it with a container and provide height and width 
                 onPressed:(){
-                  var wt=wtcontroller.text.toString();
+                  var wt=wtcontroller.text.toString();    //initially I think that we have to convert every input to string ,later convert it to whatever you want
                   var ft=ftcontroller.text.toString();
                   var inch= incontroller.text.toString();
 
                   if(wt!="" && ft!="" && inch!=""){
-                    var iWt = int.parse(wt);
+                    var iWt = int.parse(wt);        //this is how we convert any datatype to integer
                     var iFt = int.parse(ft);
                     var iInch = int.parse(inch);
 
@@ -99,9 +104,7 @@ class _HomePageState extends State<HomePage> {
                     var t3=t2/100;
                     var bmi=iWt/(t3*t3);
 
-
-                    var msg="";
-
+                    var msg="";    //the message is kept empty initially so that nothing is visible till 1st input
 
                     if(bmi>25){
                       msg="You are overweight";
@@ -113,11 +116,9 @@ class _HomePageState extends State<HomePage> {
                       msg="You are healthy";
                       bgcolor=Colors.green.shade500;
                     }
-                    setState(() {
-
-                      result="$msg \nYour BMI is : ${bmi.toStringAsFixed(2)} ";
+                    setState(() {                                                 //preferably put only those things under setState that are going to be displayed
+                      result="$msg \nYour BMI is : ${bmi.toStringAsFixed(2)} ";   //see at the bottom ,some concepts  
                     });
-
 
                   }else{
                     setState(() {
@@ -126,8 +127,7 @@ class _HomePageState extends State<HomePage> {
                   }
                 },
               ),Container(
-                height: 20,
-              ),
+                height: 20),
               Text(result,style: const TextStyle(fontSize: 24,color: Colors.black),)
             ],
           ),
@@ -137,3 +137,16 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+//to display a simple string or character
+//result="ratan";
+
+//to display a variable value with constant string
+//result="ratan is a $variable_name boy";
+
+//to perform an operation with constant string
+//result="roll no ${20+7} ";
+
+//result="ratan is a $1+2 boy"; wrong
+
